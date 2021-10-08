71c881cca0b008c0ecdba08d37230e5b74010f08
lascudaniel92 <43698270+lascudaniel92@users.noreply.github.com>
2021-10-09 02:40:19 +0300

    asd

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { FlatList, Text, TouchableOpacity } from 'react-native';
+import { FlatList, Image, Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 const data = [
   {
-    id: '0',
+    id: '1',
     author: 'Alejandro Escamilla',
     width: 5616,
     height: 3744,
     url: 'https://unsplash.com/photos/yC-Yzbqy7PY',
     download_url: 'https://picsum.photos/id/0/5616/3744',
-    title: 'First one',
+    title: 'xxx',
   },
   {
-    id: '1',
+    id: '2',
     author: 'Alejandro Escamilla',
     width: 5616,
     height: 3744,
@@ -21,13 +21,50 @@ const data = [
     title: 'Second one',
   },
   {
-    id: '10',
+    id: '3',
     author: 'Paul Jarvis',
     width: 2500,
     height: 1667,
     url: 'https://unsplash.com/photos/6J--NXulQCs',
     download_url: 'https://picsum.photos/id/10/2500/1667',
-    title: 'Third one',
+    title: 'v25jihv5 2ljv652;hov 62v62 ;h4v26 ;4h,v6 4;h6v 4',
+  },
+
+  {
+    id: '4',
+    author: 'Tina Rataj',
+    width: 2500,
+    height: 1656,
+    url: 'https://unsplash.com/photos/pwaaqfoMibI',
+    download_url: 'https://picsum.photos/id/100/2500/1656',
+    title: 'v25jihv5 2ljv652;hov 62v62 ;h4v26 ;4h,v6 4;h6v 4',
+  },
+  {
+    id: '5',
+    author: 'Lukas Budimaier',
+    width: 5626,
+    height: 3635,
+    url: 'https://unsplash.com/photos/6cY-FvMlmkQ',
+    download_url: 'https://picsum.photos/id/1000/5626/3635',
+    title: 'Fifth one',
+  },
+  {
+    id: '6',
+    author: 'Danielle MacInnes',
+    width: 5616,
+    height: 3744,
+    url: 'https://unsplash.com/photos/1DkWWN1dr-s',
+    download_url: 'https://picsum.photos/id/1001/5616/3744',
+    title: 'Sixth one',
+  },
+  {
+    id: '7',
+    author: 'NASA',
+    width: 4312,
+    height: 2868,
+    url: 'https://unsplash.com/photos/6-jTZysYY_U',
+    download_url: 'https://picsum.photos/id/1002/4312/2868',
+    title: 'Seventh one',
   },
 ];
 
@@ -35,13 +72,42 @@ const NavOptions = () => {
   return (
     <FlatList
       data={data}
+      columnWrapperStyle={{ flex: 1, justifyContent: 'center' }}
+      numColumns={2}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>{item.title}</Text>
+        <TouchableOpacity style={{}}>
+          <View style={styles.fullview}>
+            <Image style={styles.image} source={{ uri: item.download_url }} />
+            <Text numberOfLines={1} style={styles.author}>
+              {item.author}
+            </Text>
+          </View>
         </TouchableOpacity>
       )}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  fullview: {
+    borderColor: 'rgba(1, 1, 1, 0.7)',
+    borderWidth: 3,
+    margin: 3,
+    padding: 5,
+    backgroundColor: 'rgba(155, 155, 155, 0.5)',
+  },
+  author: {
+    width: 165,
+    backgroundColor: 'white',
+    color: 'black',
+    textAlign: 'center',
+    alignSelf: 'center',
+    padding: 2,
+    margin: 5,
+    fontWeight: '700',
+  },
+  image: { width: 168, height: 168, margin: 3, padding: 1 },
+});
 
 export default NavOptions;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import Constants from 'expo-constants';
+const statusBarHeight = Constants.statusBarHeight;
 
-const image = { uri: 'https://picsum.photos/1000/2000' };
+const image = { uri: 'https://picsum.photos/1000/2005' };
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={tw`bg-white h-full`}>
-      <View style={tw`p-1`}>
-        <ImageBackground resizeMode="cover" style={{ width: '100%', height: '100%' }} source={image}>
-          <Text style={styles.text}>Home Screen</Text>
+    <SafeAreaView style={{ marginTop: statusBarHeight }}>
+      <View style={{}}>
+        <ImageBackground resizeMode="cover" style={styles.imgbg} source={image}>
+          <Text style={styles.titleapp}>Home Screen</Text>
           <NavOptions />
         </ImageBackground>
       </View>
@@ -18,13 +20,9 @@ const HomeScreen = () => {
   );
 };
 
-export default HomeScreen;
-
 const styles = StyleSheet.create({
-  text: {
-    color: 'blue',
-    padding: 10,
-    fontSize: 35,
-    textAlign: 'center',
-  },
+  titleapp: { fontSize: 40, color: 'yellow', textAlign: 'center', backgroundColor: 'rgba(1, 1, 1, 0.7)' },
+  imgbg: { width: '100%', height: '100%' },
 });
+
+export default HomeScreen;
